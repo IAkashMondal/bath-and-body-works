d41f15775b731871720dd5b9a07944e4e505ec41
Naresh Mewada <nareshmewada014@gmail.com>
2022-05-08 13:17:44 +0530

    Product by Naresh Rajput

---
+++ footer res/footer.js
@@ -0,0 +1,16 @@
+import  {down} from "/bath-and-body-works-2/footer res/script/myfooter.js";
+let contain = document.querySelector(".footer");
+contain.innerHTML = down();
+
+
+let footer_click = () => {
+
+    let b = document.querySelector("#footer_confirm").value
+    if(b===""){
+       
+      let show =  document.querySelector("#show2");
+      show.innerHTML = "this field is required.";
+      show.style.color = "red"
+    }
+}
+
